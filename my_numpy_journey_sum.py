34530022dad09c954508e9b3dba7585cbbd0787e
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-25 18:40:21 +0100

    first commit

---
+++ my_numpy_journey_sum.py
@@ -0,0 +1,4 @@
+#TASK: Create a function which sum a numpy array. It will receive one numpy array and return the result of the operation.
+import numpy as np 
+def my_numpy_journey_sum(np_array):
+    return np.sum(np_array)
